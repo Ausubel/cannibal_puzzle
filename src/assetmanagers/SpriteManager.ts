0bd6a5d0b7c9a041416776690497afa3cfa0a31b
Ausubel <ausubelvc@gmail.com>
2024-01-02 20:29:34 -0500

    adding cannibal sprites and updating sprite manager

--- src/assetmanagers/SpriteManager.ts
+++ src/assetmanagers/SpriteManager.ts
@@ -1,26 +1,9 @@
-export type EnemySprites = {
-    moving: HTMLImageElement[];
-    fire: HTMLImageElement;
-};
-
 export default class SpriteManager {
     readonly PLAYER_SHIP_1: HTMLImageElement[];
-    readonly PLAYER_SHIP_2: HTMLImageElement[];
     readonly PLAYER_SHIP_1_LASER: HTMLImageElement;
-    readonly PLAYER_SHIP_2_LASER: HTMLImageElement;
-    readonly ENEMY_LASER: HTMLImageElement;
-    readonly WEAK_ENEMY: EnemySprites;
-    readonly REGULAR_ENEMY: EnemySprites;
-    readonly STRONG_ENEMY: EnemySprites;
     private constructor() {
         this.PLAYER_SHIP_1 = this.getPlayerShipSprites("ship_1");
-        this.PLAYER_SHIP_2 = this.getPlayerShipSprites("ship_2");
         this.PLAYER_SHIP_1_LASER = this.createImage("/players/ship_1/laser_ship_1/0.png");
-        this.PLAYER_SHIP_2_LASER = this.createImage("/players/ship_2/laser_ship_2/0.png");
-        this.ENEMY_LASER = this.createImage("/enemies/enemy_laser/0.png");
-        this.WEAK_ENEMY = this.getEnemySprites("enemy_weak");
-        this.REGULAR_ENEMY = this.getEnemySprites("enemy_regular");
-        this.STRONG_ENEMY = this.getEnemySprites("enemy_strong");
     }
     static create(): Promise<SpriteManager> {
         return new Promise<SpriteManager>(res => {
@@ -35,37 +18,13 @@ export default class SpriteManager {
     private toImageList(): HTMLImageElement[] {
         const {
             PLAYER_SHIP_1,
-            PLAYER_SHIP_2,
             PLAYER_SHIP_1_LASER,
-            PLAYER_SHIP_2_LASER,
-            ENEMY_LASER,
-            WEAK_ENEMY,
-            REGULAR_ENEMY,
-            STRONG_ENEMY,
         } = this;
         return [
             ...PLAYER_SHIP_1,
-            ...PLAYER_SHIP_2,
             PLAYER_SHIP_1_LASER,
-            PLAYER_SHIP_2_LASER,
-            ENEMY_LASER,
-            ...WEAK_ENEMY.moving,
-            WEAK_ENEMY.fire,
-            ...REGULAR_ENEMY.moving,
-            REGULAR_ENEMY.fire,
-            ...STRONG_ENEMY.moving,
-            STRONG_ENEMY.fire,
         ]
     };
-    private getEnemySprites(pathEnemy: string): EnemySprites {
-        const enemy: EnemySprites = {
-            fire: this.createImage(`/enemies/${pathEnemy}/fire.png`),
-            moving: Array(3)
-        };
-        for (let i = 0; i < enemy.moving.length; i++) 
-            enemy.moving[i] = this.createImage(`/enemies/${pathEnemy}/${i}.png`);
-        return enemy;
-    }
     private getPlayerShipSprites(pathPlayer: string): HTMLImageElement[] {
         const frames = Array(3);
         for (let i = 0; i < frames.length; i++) 
